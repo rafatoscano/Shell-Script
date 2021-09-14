483c7de84854c03da97913d493d71f239fd61ded
Rafael Toscano <rafael.toscano@academico.ifpb.edu.br>
2021-09-14 02:02:23 -0300

    Telegram completo

---
+++ Lista-2/Q11.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+case $1 in (logica) echo "test sobre logica" ;; (aritmetrica) echo "test sobre aritmetrica" ;; (string) echo "test sobre string" ;; (variaveis) echo "test sobre variaveis" ;; (arquivos) echo "test sobre arquivos" ;;  *) echo "Opcao Invalida!"
+esac
